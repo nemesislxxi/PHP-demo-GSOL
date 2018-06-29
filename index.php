e0989b12bf7f17759710e5bcabfe91feefa39244
nemesislxxi <roy.white11@outlook.com>
2018-06-29 21:58:57 +0100

    New code

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 
 function ServiceHandler() {
     $data = new stdClass();
-    $data->Output = "Hello World!";
+    $data->Output = "Good Afternoon GSOL - This is a very basic PHP example!";
 
     return json_encode($data);
 };
